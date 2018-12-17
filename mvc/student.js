e178d5bcaddaecae17e213b66f11a9ad47c360e9
laurentbubba <34655225+laurentbubba@users.noreply.github.com>
2018-12-17 13:29:06 +0100

    yep
    iedereen die arno heet is gay

--- mvc/student.js
+++ mvc/student.js
@@ -1,1 +1,44 @@
 // Schrijf hier je code
+
+function onMouseDown(state, args){
+    return state+1;
+}
+
+function onMouseDown2(state, args){
+    return {count: state.count+1};
+}
+
+function counter3(){
+    function onMouseDown(state, args){
+        return {count: state.count+1};
+    }
+    return {controller: {onMouseDown}};
+}
+
+function counter4() {
+    function onMouseDown(state, args) {
+        return {count: state.count+1};
+    }
+    function onKeyDown({count}, args){
+        return {count: 0};
+    }
+    return { controller: {onMouseDown, onKeyDown} };
+}
+
+function counter5(){
+    function onMouseDown(state, args) {
+        if (args.shift){
+            if (state.count === 0){
+                return {count: state.count};
+            } else {
+                return {count: state.count - 1};
+            }
+        } else if(!args.shift){
+            return {count: state.count+1};
+        }
+    }
+    function onKeyDown(){
+        return;
+    }
+    return {controller: {onMouseDown, onKeyDown}}
+}
