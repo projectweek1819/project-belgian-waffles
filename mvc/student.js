de39044c5d5f7f8cd187af2b1a1818072c8fe17b
arnobarre <43341295+arnobarre@users.noreply.github.com>
2018-12-18 11:01:20 +0100

    mooi
    kkk

--- mvc/student.js
+++ mvc/student.js
@@ -5,7 +5,7 @@ function onMouseDown(state, args){
 }
 
 function onMouseDown2(state, args){
-    return {count: state.count+1};
+    return {count: state.count+1}; snotjes
 }
 
 function counter3(){
